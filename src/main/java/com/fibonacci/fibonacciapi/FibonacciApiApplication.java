7e8b1a035cff70b45a7cc0756d6c22f4df3169ec
Roger Diller <roger@flexrentalsolutions.com>
2020-10-22 09:15:13 -0400

    initial commit right after start.spring.io init

---
+++ src/main/java/com/fibonacci/fibonacciapi/FibonacciApiApplication.java
@@ -0,0 +1,13 @@
+package com.fibonacci.fibonacciapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FibonacciApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FibonacciApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fibonacci/fibonacciapi/FibonacciApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fibonacci.fibonacciapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FibonacciApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
