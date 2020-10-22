c8e9e683faade7732e9fef4761ce405ac3796c01
Roger Diller <roger@flexrentalsolutions.com>
2020-10-22 10:12:07 -0400

    spaces

--- src/main/java/com/fibonacci/fibonacciapi/FibonacciController.java
+++ src/main/java/com/fibonacci/fibonacciapi/FibonacciController.java
@@ -54,13 +54,19 @@ public class FibonacciController {
     }
 
     public long fibonacci(long n) {
+
         long a = 0, b = 1, c;
-        if (n == 0) return a;
+
+        if (n == 0){
+            return a;
+        }
+
         for (int i = 2; i <= n; i++) {
             c = a + b;
             a = b;
             b = c;
         }
+
         return b;
     }
 
