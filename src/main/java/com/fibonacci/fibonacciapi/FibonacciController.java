79af59e6ab5b48f19bba778369060974b4c018e8
Roger Diller <roger@flexrentalsolutions.com>
2020-10-22 09:20:56 -0400

    stubbed in FibonacciController

---
+++ src/main/java/com/fibonacci/fibonacciapi/FibonacciController.java
@@ -0,0 +1,24 @@
+package com.fibonacci.fibonacciapi;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FibonacciController {
+
+    @GetMapping("/current")
+    public long current(){
+        return 0;
+    }
+
+    @GetMapping("/current")
+    public long next(){
+        return 1;
+    }
+
+    @GetMapping("/current")
+    public long previous(){
+        return 0;
+    }
+
+}
