ac570d002317197df0625f03f01bec392f8de8bf
Roger Diller <roger@flexrentalsolutions.com>
2020-10-22 09:38:52 -0400

    fibonacci method

--- src/main/java/com/fibonacci/fibonacciapi/FibonacciController.java
+++ src/main/java/com/fibonacci/fibonacciapi/FibonacciController.java
@@ -6,19 +6,51 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class FibonacciController {
 
+    private int counter = 0;
+
+    private int currentFibSequence = 0;
+
     @GetMapping("/current")
-    public long current(){
-        return 0;
+    public int current(){
+        return currentFibSequence;
     }
 
     @GetMapping("/next")
-    public long next(){
-        return 1;
+    public int next(){
+
+        counter++;
+
+        currentFibSequence = fibonacciRecursiveMem(counter);
+
+        return currentFibSequence;
     }
 
     @GetMapping("/previous")
-    public long previous(){
-        return 0;
+    public int previous(){
+
+        counter--;
+
+        if(counter < 0){
+            return currentFibSequence;
+        }
+
+        currentFibSequence = fibonacciRecursiveMem(counter);
+
+        return currentFibSequence;
+    }
+
+    public int fibonacciRecursiveMem(int n) {
+        return fibonacciRecursiveMem(n, new int[n+1]);
+    }
+
+    public int fibonacciRecursiveMem(int i, int mem[]) {
+        if (i==0 || i==1) return i;
+
+        if (mem[i] == 0) {
+            mem[i] = fibonacciRecursiveMem(i-1, mem) + fibonacciRecursiveMem(i-2, mem);
+        }
+
+        return mem[i];
     }
 
 }
