0e4da6f8bff23592dea6de2bca6d4f766e1c1ab6
Roger Diller <roger@flexrentalsolutions.com>
2020-10-24 14:02:24 -0400

    FibonacciControllerTest betterment

--- src/test/java/com/fibonacci/fibonacciapi/FibonacciControllerTest.java
+++ src/test/java/com/fibonacci/fibonacciapi/FibonacciControllerTest.java
@@ -1,10 +1,14 @@
 package com.fibonacci.fibonacciapi;
 
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class FibonacciControllerTest {
+
+    private static final Logger LOG = LoggerFactory.getLogger(FibonacciControllerTest.class);
 
     FibonacciController tested = new FibonacciController();
 
@@ -13,14 +17,15 @@ class FibonacciControllerTest {
 
         long current = System.currentTimeMillis();
         long timeTaken;
+        long ops = 10000;
 
-        for(int i = 0; i < 3000; i++){
+        for(int i = 0; i < ops; i++){
             tested.next();
         }
 
         timeTaken = System.currentTimeMillis() - current;
 
-        System.out.println("Time taken to compute it : " + timeTaken + " milliseconds");
+        LOG.info("Time taken to compute fibonanci {} NEXT ops | {} milliseconds", ops, timeTaken);
     }
 
     @Test
@@ -28,15 +33,15 @@ class FibonacciControllerTest {
 
         long current = System.currentTimeMillis();
         long timeTaken;
+        long ops = 10000;
 
-        for(int i = 0; i < 3000; i++){
+        for(int i = 0; i < ops; i++){
             tested.previous();
         }
 
         timeTaken = System.currentTimeMillis() - current;
 
-        System.out.println("Time taken to compute it : " + timeTaken + " milliseconds");
-
+        LOG.info("Time taken to compute fibonanci {} PREVIOUS ops | {} milliseconds", ops, timeTaken);
     }
 
     @Test
